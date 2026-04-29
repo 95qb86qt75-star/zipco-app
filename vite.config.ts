ba2ae2efa8c7257166d4e574fec845699a3d7979
95qb86qt75-star <95qb86qt75@privaterelay.appleid.com>
2026-04-28 23:10:30 -0400

    Add defensive locationSearch and setter state in App

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
